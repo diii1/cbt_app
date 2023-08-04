602dee0ca7ad20689929aed7525de8f0a7b4d5f8
Raviy Bayu <bayu.setiaji709@gmail.com>
2023-08-04 21:29:45 +0700

    chore(database): create migration and model session

---
+++ app/Models/Session.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Session extends Model
+{
+    use HasFactory;
+
+    protected $table = "sessions";
+    protected $primaryKey = "id";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'time_start',
+        'time_end',
+    ];
+}
