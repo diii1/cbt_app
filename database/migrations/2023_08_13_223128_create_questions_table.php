727e7f71e42bc0a8056d65c20cd934e29b5397ca
Raviy Bayu <bayu.setiaji709@gmail.com>
2023-08-14 19:18:42 +0700

    chore: add migration for question table

---
+++ database/migrations/2023_08_13_223128_create_questions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('exam_id');
+            $table->foreign('exam_id')->references('id')->on('exams')->onUpdate('cascade')->onDelete('restrict');
+            $table->unsignedBigInteger('subject_id');
+            $table->foreign('subject_id')->references('id')->on('subjects')->onUpdate('cascade')->onDelete('restrict');
+            $table->string('exam_title');
+            $table->string('subject_name');
+            // $table->integer('number');
+            $table->longText('question');
+            $table->longText('options');
+            $table->longText('answer');
+            $table->bigInteger('created_by');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+};
