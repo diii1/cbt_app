13251d9d3e82405d99c96a1ebaba314b42ef42c6
Raviy Bayu <bayu.setiaji709@gmail.com>
2023-09-23 01:19:36 +0700

    chore: bypass access to public folder

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Laravel - A PHP Framework for web Artisan 
+ * 
+ * @package Laravel
+ * @author Admin@CBT.mts
+ */
+
+$url = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)    
+);
+
+if ($url !== '/' && file_exists(__DIR__.'/public'.$url)){
+    return false;
+}
+
+require_once __DIR__.'/public/index.php';
