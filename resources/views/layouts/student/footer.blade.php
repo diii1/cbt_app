4f0fbc0c1342eee169afc6d4143b00f0a3b659fb
Raviy Bayu <bayu.setiaji709@gmail.com>
2023-09-24 20:47:19 +0700

    refactor: footer layout student missing school name

--- resources/views/layouts/student/footer.blade.php
+++ resources/views/layouts/student/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="p-0 d-flex justify-content-center {{ $profile->background ? 'bg-light' : '' }} fixed-bottom">
-    Copyright © {{ date('Y') }} &mdash; MTs Faqih Hasyim.
+    Copyright © {{ date('Y') }} &mdash; {{ $profile->name }}.
 </footer>
